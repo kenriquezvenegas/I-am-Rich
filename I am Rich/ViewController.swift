1aa680a3041df7260468c15493cc1eca38d0a5ce
Beatriz Enriquez <kenriquezvenegas@deloitte.com>
2024-01-22 19:13:44 -0600

    Initial commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Beatriz Enríquez on 04/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
